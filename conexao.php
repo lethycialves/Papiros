aa4385eb0462cfe24c52281f4635108249ff41ac
Lethycia Alves <lethycia@email.com>
2026-07-28 20:02:10 -0300

    mudando de MV para ML

--- conexao.php
+++ conexao.php
@@ -1,8 +1,21 @@
 <?php
-
+/*MÁQUINA VIRTUAL
 $host = "192.168.56.10";
 $user = "lethycia";
 $senha = "789456";
+$db = "projeto_papiros";
+
+$conexao = new mysqli($host, $user, $senha, $db);
+
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
+}
+*/
+
+/*MÁQUINA LOCAL*/
+$host = "localhost";
+$user = "root";
+$senha = "";
 $db = "projeto_papiros";
 
 $conexao = new mysqli($host, $user, $senha, $db);
